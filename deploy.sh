b6600ebb79ef90178921d9550620e499bf8bb7e7
jeevanBrillianceit <90598943+jeevanBrillianceit@users.noreply.github.com>
2024-02-01 16:02:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,20 @@ export EC2_PRIVATE_KEY="~/.ssh/cicdpipeline.pem"
 export EC2_USERNAME="ubuntu"
 export EC2_HOST="ec2-3-93-212-44.compute-1.amazonaws.com"
 
-# SSH into the EC2 instance and execute any additional deployment steps
+# SSH into the EC2 instance and execute deployment steps
 ssh -o StrictHostKeyChecking=no -i $EC2_PRIVATE_KEY $EC2_USERNAME@$EC2_HOST << 'ENDSSH'
-  cd /home/ubuntu/my-node-app
+  # Navigate to the Node.js project directory
+  cd /path/to/your/Node.js/project
 
-  # Update the Node.js project from GitHub
-  git pull origin main  # Assuming your project is on GitHub
+  # Pull latest changes from GitHub
+  git pull origin main
 
-  # Install dependencies
+  # Install Node.js dependencies
   npm install
 
-  # Restart the Node.js application
-  npm start  # Adjust as needed based on your start command
+  # Restart the Node.js application (replace with your actual start command)
+  pm2 restart my-node-app
+
+  # Update Nginx configuration (replace with your actual configuration update)
+  sudo nginx -s reload
 ENDSSH
